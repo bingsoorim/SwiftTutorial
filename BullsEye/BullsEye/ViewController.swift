cf86e9fab1a4d3803e773afed66120a813ae1279
srim <srim@sfu.ca>
2018-11-01 16:27:48 -0700

    Create a function

--- BullsEye/BullsEye/ViewController.swift
+++ BullsEye/BullsEye/ViewController.swift
@@ -6,15 +6,26 @@
 //  Copyright © 2018 Soobin Rim. All rights reserved.
 //
 
+// viewWillAppear -> viewDidAppear -> viewWillDisappear -> viewDidDisappear
+
 import UIKit
-
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    @IBAction func showAlert() {
+        let alert = UIAlertController(title: "Hello, World",
+                                      message: "This is my first app!",
+                                      preferredStyle: .alert)
+        let action = UIAlertAction(title: "Awesome", style: .default,
+                                   handler: nil)
+        alert.addAction(action)
+        // self.present -> the function that view controller has
+        present(alert, animated: true, completion: nil)
+    }
 }
-
