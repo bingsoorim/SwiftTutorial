601a6e6a5d9cc233c4067bc1194db7aac3823679
srim <srim@sfu.ca>
2018-11-01 16:13:21 -0700

    Create a project

---
+++ BullsEye/BullsEye/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BullsEye
+//
+//  Created by Soobin Rim on 2018-11-01.
+//  Copyright © 2018 Soobin Rim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
