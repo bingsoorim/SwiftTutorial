020f4795cc8c555739f4e5bc9ea6e671ff1ee2a0
srim <srim@sfu.ca>
2018-11-08 17:18:40 -0800

    Add some modifications

--- BullsEye/BullsEye/ViewController.swift
+++ BullsEye/BullsEye/ViewController.swift
@@ -10,19 +10,22 @@
 
 import UIKit
 class ViewController: UIViewController {
-    
+    @IBOutlet weak var slider: UISlider!
     var currentValue: Int = 50
+    var targetValue: Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        targetValue = 1 + Int(arc4random_uniform(100))
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     @IBAction func showAlert() {
-        let message = "The value of the slider is: \(currentValue)"
+        let message = "The value of the slider is: \(currentValue)" +
+        "\nThe target value is: \(targetValue)"
         
         let alert = UIAlertController(title: "Hello, World",
                                       message: message,
